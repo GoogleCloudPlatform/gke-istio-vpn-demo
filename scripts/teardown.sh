498b49107507c4389d145cb828dd3a9d3f503777
Ala Raddaoui <raddaouiala@gmail.com>
2019-03-25 14:53:34 -0700

    remove exit on error for teardown script

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
 set -x
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
